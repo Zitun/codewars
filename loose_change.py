6c7c01a90bf597503f21e9230faf99a5d805331f
Zitun <olivierdarrasse@gmail.com>
2021-02-02 15:49:04 +0000

    Update loose_change.py

--- loose_change.py
+++ loose_change.py
@@ -1,20 +1,10 @@
 def loose_change(cents):
 
-    change_dict = {
-        'Quarters': 0,
-        'Dimes': 0,
-        'Nickels': 0,
-        'Pennies': 0
-    }
+    change_dict = {'Quarters': 0, 'Dimes': 0, 'Nickels': 0, 'Pennies': 0}
 
     if isinstance(cents, (float,int)) and cents > 0 :
 
-        conversion_dict = {
-            'Quarters': 25,
-            'Dimes': 10,
-            'Nickels': 5,
-            'Pennies': 1
-        }
+        conversion_dict = {'Quarters': 25, 'Dimes': 10, 'Nickels': 5, 'Pennies': 1}
 
         remainder = int(cents)
 
